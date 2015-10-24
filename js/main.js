2bd2e2f2fba9022f88161e5aca9afc36a179055d
Diddier Hilarion <user@windows>
2015-10-24 02:19:40 -0500

    updated emmet

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function clickCell(node){
 	displayNumberChooser();	
 	document.clicked = node;
@@ -17,4 +16,28 @@ function displayNumberChooser(){
 function hideNumberChooser(){
 	node = document.getElementsByClassName("placeholder")[0];
 	node.style["display"]="none";
+}
+
+function clickMenu(){
+	switchMenu();
+}
+
+function switchMenu(){
+	node = document.getElementsByClassName("menu")[0];
+	if(node.style["display"] == "none"){
+		node.style["display"] = "flex";
+	}
+	else{
+		node.style["display"] = "none";
+	}
+}
+
+function checkValidGroup(group){
+	//group is an array of nodes which can represent a row, column or inner square
+}
+function initializeGroups(){
+	//this function should initialize all groups (lines, rows and inner squares and store them somewhere)
+}
+function checkBoard(){
+	//this function should run checkValidGroup on every group available if all groups are valid the game is over 
 }
